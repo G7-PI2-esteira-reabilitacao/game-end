39948ada27100f580cf16b1a33ca827db5c26fe8
Julio Litwin <j.litwin@live.com>
2021-09-05 13:53:04 -0300

    Added first project version.
    - Included with OpenPose;
    - Included Initial gameplay;

---
+++ OpenPosePlugin/Assets/Game/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    
+    // offset between camera and target
+    public Vector3 _offset;
+    
+    // This value will change at the runtime depending on target movement. Initialize with zero vector.
+    private Vector3 velocity = Vector3.zero;
+    
+    // change this value to get desired smoothness
+    public float SmoothTime = 0.3f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - _target.position;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        // update position
+        //Vector3 targetPosition = _target.position + Offset;
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, Time.deltaTime);
+ 
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, _offset.z + _target.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.6f);
+    }
+}
---
+++ OpenPosePlugin/Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    static int s_DeadHash     = Animator.StringToHash("Dead");
+    static int s_RunStartHash = Animator.StringToHash("runStart");
+    static int s_MovingHash   = Animator.StringToHash("Moving");
+
+    private CharacterController _controller;
+    private Animator            _animator;
+    
+    private Vector3             _direction;
+    private float               _forwardSpeed = 6f;
+
+    private int   _desiredLane    = 1;
+    private float _laneDistance   = 1f;
+    public  float laneChangeSpeed = 1.0f;
+    
+    protected Vector3 m_TargetPosition = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller        = gameObject.AddComponent<CharacterController>();
+        _controller.height = 1.0f;
+        
+        _animator          = gameObject.GetComponent<Animator>();
+
+        StartRunning();
+    }
+    
+    public void StartRunning()
+    {
+        if (_animator)
+        {
+            _animator.Play(s_RunStartHash);
+            _animator.SetBool(s_MovingHash, true);
+        }
+    }
+    
+    public void StopMoving()
+    {
+        if (_animator)
+        {
+            _animator.SetBool(s_MovingHash, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.z = _forwardSpeed;
+
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _desiredLane++;
+            if (_desiredLane == 2)
+                _desiredLane = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _desiredLane--;
+            if (_desiredLane == -1)
+                _desiredLane = 0;
+        }
+        //m_TargetPosition = new Vector3((_desiredLane - 1) * _laneDistance, 0, 0);
+        var position = transform.position.z               * transform.forward + transform.position.y * transform.up;
+
+        switch (_desiredLane)
+        {
+            case 0 :
+                position += Vector3.left * _laneDistance;
+                break;
+            case 1 :
+                position += Vector3.right * _laneDistance;
+                break;
+        }
+        
+        transform.position = Vector3.Lerp(transform.position, position, 80 * Time.deltaTime);
+        
+        _controller.Move(_direction * Time.deltaTime);
+        //Vector3 verticalTargetPosition = m_TargetPosition;
+        //transform.localPosition = Vector3.MoveTowards(transform.localPosition, verticalTargetPosition, laneChangeSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
